615c58ffdaf9a0079e3e5b96fef308b630a75316
ashishsonihg <86300442+ashishsonihg@users.noreply.github.com>
2021-07-05 17:14:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+var btn = document.querySelector('.add');
+var remove = document.querySelector('.draggable');
+
+function dragStart(e) {
+  this.style.opacity = '0.4';
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+};
+
+function dragEnter(e) {
+  this.classList.add('over');
+}
+
+function dragLeave(e) {
+  e.stopPropagation();
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function dragDrop(e) {
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  return false;
+}
+
+function dragEnd(e) {
+  var listItens = document.querySelectorAll('.draggable');
+  [].forEach.call(listItens, function(item) {
+    item.classList.remove('over');
+  });
+  this.style.opacity = '1';
+}
+
+function addEventsDragAndDrop(el) {
+  el.addEventListener('dragstart', dragStart, false);
+  el.addEventListener('dragenter', dragEnter, false);
+  el.addEventListener('dragover', dragOver, false);
+  el.addEventListener('dragleave', dragLeave, false);
+  el.addEventListener('drop', dragDrop, false);
+  el.addEventListener('dragend', dragEnd, false);
+}
+
+var listItens = document.querySelectorAll('.draggable');
+[].forEach.call(listItens, function(item) {
+  addEventsDragAndDrop(item);
+});
+
+function addNewItem() {
+  var newItem = document.querySelector('.input').value;
+  if (newItem != '') {
+    document.querySelector('.input').value = '';
+    var li = document.createElement('li');
+    var attr = document.createAttribute('draggable');
+    var ul = document.querySelector('ul');
+    li.className = 'draggable';
+    attr.value = 'true';
+    li.setAttributeNode(attr);
+    li.appendChild(document.createTextNode(newItem));
+    ul.appendChild(li);
+    addEventsDragAndDrop(li);
+  }
+}
+
+btn.addEventListener('click', addNewItem);
+
+
+
+
+
+
+function saveList(){
+  var listItens = document.getElementsByClassName('draggable');
+  localStorage.setItem("count",listItens.length);
+  for(let i=0;i<listItens.length;i++){
+    localStorage.setItem(i,listItens[i].innerHTML);
+  }
+}
+
+function clearList()
+{
+  localStorage.clear();
+  populatePage();
+}
+
+function populatePage(){
+  let pop=document.getElementById('lis');
+  let storedValues=""
+  for(let i=0;i<localStorage.getItem('count');i++){
+    storedValues+="<li class='draggable' draggable='true'>"+localStorage.getItem(i)+"</li>";
+  }
+
+  pop.innerHTML=storedValues;
+  var listItens = document.querySelectorAll('.draggable');
+  [].forEach.call(listItens, function(item) {
+    addEventsDragAndDrop(item);
+  });
+
+}
+
+function saveItem(){
+
+}
